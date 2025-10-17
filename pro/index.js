950f93784646aceb8fae43a36a3bda49d7e6d7d4
A7medRabie <a.rabie1001@gmail.com>
2025-10-17 23:29:25 +0300

    ffff

---
+++ pro/index.js
@@ -0,0 +1,665 @@
+
+const x=10
+let arr=[a,b]
+const x=50
+     questions=[
+      {
+        answers: [
+           
+          {
+            answer: "Home Tool Markup Language",
+            key: "A3"
+          }
+        ],
+        type: "single_choice",
+        _id: "670082800a5849a4aee16294",
+        question: "What does HTML stand for?",
+        correct: "A2",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:04:16.070Z"
+      },
+      {
+        answers: [
+          {
+            answer: "Microsoft",
+            key: "A1"
+          },
+          {
+            answer: "Google",
+            key: "A2"
+          },
+          {
+            answer: "Mozilla",
+            key: "A3"
+          },
+          {
+            answer: "The World Wide Web Consortium",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "6700829e0a5849a4aee16297",
+        question: "Who is making the Web standards?",
+        correct: "A4",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:04:46.157Z"
+      },
+      {
+        answers: [
+          {
+            answer: "<head>",
+            key: "A1"
+          },
+          {
+            answer: "<heading>",
+            key: "A2"
+          },
+          {
+            answer: "<h1>",
+            key: "A3"
+          },
+          {
+            answer: "<h6>",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "670082e50a5849a4aee1629a",
+        question: "Choose the correct HTML element for the largest heading",
+        correct: "A3",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:05:57.915Z"
+      },
+      {
+        answers: [
+          {
+            answer: "<br>",
+            key: "A1"
+          },
+          {
+            answer: "<lb>",
+            key: "A2"
+          },
+          {
+            answer: "<break>",
+            key: "A3"
+          },
+          {
+            answer: "<pre>",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "6700831e0a5849a4aee1629d",
+        question: "What is the correct HTML element for inserting a line break?",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:06:54.629Z"
+      },
+      {
+        answers: [
+          {
+            answer: "<body style='background-color:yellow;'>",
+            key: "A1"
+          },
+          {
+            answer: "<body bg='yellow'>",
+            key: "A2"
+          },
+          {
+            answer: "<background>yellow</background>",
+            key: "A3"
+          },
+          {
+            answer: "<yellow>yellow</yellow>",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "670083cb0a5849a4aee162a0",
+        question: "What is the correct HTML for adding a background color?",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:09:47.386Z"
+      },
+      {
+        answers: [
+          {
+            answer: "<important>",
+            key: "A1"
+          },
+          {
+            answer: "<i>",
+            key: "A2"
+          },
+          {
+            answer: "<b>",
+            key: "A3"
+          },
+          {
+            answer: "<strong>",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "670084450a5849a4aee162a3",
+        question: "Choose the correct HTML element to define important text",
+        correct: "A4",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:11:49.989Z"
+      },
+      {
+        answers: [
+          {
+            answer: "<em>",
+            key: "A1"
+          },
+          {
+            answer: "<italic>",
+            key: "A2"
+          },
+          {
+            answer: "<i>",
+            key: "A3"
+          },
+          {
+            answer: "<strong>",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "670084870a5849a4aee162a6",
+        question: "Choose the correct HTML element to define emphasized text",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:12:55.227Z"
+      },
+      {
+        answers: [
+          {
+            answer: "<a href='http://www.w3schools.com'>W3Schools</a>",
+            key: "A1"
+          },
+          {
+            answer: "<a>http://www.w3schools.com</a>",
+            key: "A2"
+          },
+          {
+            answer: "<a url='http://www.w3schools.com'>W3Schools.com</a>",
+            key: "A3"
+          },
+          {
+            answer: "<a name='http://www.w3schools.com'>W3Schools.com</a>",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "6700851d0a5849a4aee162a9",
+        question: "What is the correct HTML for creating a hyperlink?",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:15:25.980Z"
+      },
+      {
+        answers: [
+          {
+            answer: "/",
+            key: "A1"
+          },
+          {
+            answer: "*",
+            key: "A2"
+          },
+          {
+            answer: "<",
+            key: "A3"
+          },
+          {
+            answer: "~",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        _id: "6700854c0a5849a4aee162ac",
+        question: "Which character is used to indicate an end tag?",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:16:12.736Z"
+      },
+      {
+        answers: [
+          {
+            answer: "<a href='url' new>",
+            key: "A1"
+          },
+          {
+            answer: "<a href='url' target='new'>",
+            key: "A2"
+          },
+          {
+            answer: "<a href='url' target='_blank'>",
+            key: "A3"
+          }
+        ],
+        type: "single_choice",
+        _id: "670085a00a5849a4aee162af",
+        question: "How can you open a link in a new tab/browser window?",
+        correct: "A3",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-10-05T00:17:36.547Z"
+      },
+      {
+        _id: "6765c8fccc3deba60d0456cb",
+        question: "How can you op?",
+        answers: [
+          {
+            answer: "<a href='url' new>",
+            key: "A1"
+          },
+          {
+            answer: "<a href='url' target='new'>",
+            key: "A2"
+          },
+          {
+            answer: "<a href='url' target='_blank'>",
+            key: "A3"
+          },
+          {
+            answer: "~",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        correct: "A3",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2024-12-20T19:43:56.096Z"
+      },
+      {
+        _id: "68064c1f5554b328912a20eb",
+        question: "add questions input",
+        answers: [
+          {
+            answer: "a",
+            key: "A1"
+          },
+          {
+            answer: "b",
+            key: "A2"
+          },
+          {
+            answer: "c",
+            key: "A3"
+          },
+          {
+            answer: "d",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-21T13:46:07.553Z"
+      },
+      {
+        _id: "68065cfe5554b328912a279c",
+        question: "add questions inputs",
+        answers: [
+          {
+            answer: "a",
+            key: "A1"
+          },
+          {
+            answer: "b",
+            key: "A2"
+          },
+          {
+            answer: "c",
+            key: "A3"
+          },
+          {
+            answer: "d",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-21T14:58:06.707Z"
+      },
+      {
+        _id: "68065d1f5554b328912a279f",
+        question: "add questions inputss",
+        answers: [
+          {
+            answer: "a",
+            key: "A1"
+          },
+          {
+            answer: "b",
+            key: "A2"
+          },
+          {
+            answer: "c",
+            key: "A3"
+          },
+          {
+            answer: "d",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        correct: "A2",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-21T14:58:39.704Z"
+      },
+      {
+        _id: "68065ef25554b328912a27cc",
+        question: "add questions inpu",
+        answers: [
+          {
+            answer: "a",
+            key: "A1"
+          },
+          {
+            answer: "b",
+            key: "A2"
+          },
+          {
+            answer: "c",
+            key: "A3"
+          },
+          {
+            answer: "d",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        correct: "A2",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-21T15:06:26.247Z"
+      },
+      {
+        _id: "6806a9355554b328912a3810",
+        question: "Hnoogg",
+        answers: [
+          {
+            answer: "djjg",
+            key: "A1"
+          },
+          {
+            answer: "fjg",
+            key: "A2"
+          },
+          {
+            answer: "gg",
+            key: "A3"
+          }
+        ],
+        type: "single_choice",
+        correct: "A3",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-21T20:23:17.038Z"
+      },
+      {
+        _id: "6806a9455554b328912a3813",
+        question: "Hnooyyfgg",
+        answers: [
+          {
+            answer: "g",
+            key: "A1"
+          },
+          {
+            answer: "g",
+            key: "A2"
+          },
+          {
+            answer: "g",
+            key: "A3"
+          }
+        ],
+        type: "single_choice",
+        correct: "A3",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-21T20:23:33.187Z"
+      },
+      {
+        _id: "68076d685554b328912a563e",
+        question: "add new question for exam",
+        answers: [
+          {
+            answer: "a",
+            key: "A1"
+          },
+          {
+            answer: "b",
+            key: "A2"
+          },
+          {
+            answer: "c",
+            key: "A3"
+          },
+          {
+            answer: "d",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        correct: "A2",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-22T10:20:24.179Z"
+      },
+      {
+        _id: "6808cc465554b328912af100",
+        question: "Which HTML tag is used to define an unordered list?",
+        answers: [
+          {
+            answer: "<ul>",
+            key: "A1"
+          },
+          {
+            answer: "<ol>",
+            key: "A2"
+          },
+          {
+            answer: "<li>",
+            key: "A3"
+          },
+          {
+            answer: "<list>",
+            key: "A4"
+          }
+        ],
+        type: "single_choice",
+        correct: "A1",
+        subject: null,
+        exam: {
+          _id: "670070a830a3c3c1944a9c63",
+          title: "HTML Quiz",
+          duration: 25,
+          subject: "670037f6728c92b7fdf434fc",
+          numberOfQuestions: 40,
+          active: true,
+          createdAt: "2024-10-04T22:48:08.842Z"
+        },
+        createdAt: "2025-04-23T11:17:26.782Z"
+      }
+    ]
+  
+let n= 
+questions.map((question, index) => ({
+    answers: question.answers.map((answer) => ({
+      answer: answer.answer,
+      key: answer.key,
+    })),
+    _id: question._id,
+    question: question.question,
+    correct: question.correct,
+    subject: question.subject,
+    index: index,
+  }))
+ 
+console.log(n);
+
+
